a917f9c1b1bd1afac89014302874f6b2fe8cfd6d
Youngmi <youngmi9347@gmail.com>
2020-09-30 23:38:34 +0900

    Momentum 기본

---
+++ todo.js
@@ -0,0 +1,60 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODO_LS = "toDo";
+let toDo = [];
+
+function saveToDo() {
+  localStorage.setItem(TODO_LS, JSON.stringify(toDo));
+}
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDo = toDo.filter(function (toDoInput) {
+    return toDoInput.id !== parseInt(li.id);
+  });
+  toDo = cleanToDo;
+  saveToDo();
+}
+function colorToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newId = toDo.length + 1;
+  delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
+  span.innerText = text;
+  li.id = newId;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDo.push(toDoObj);
+  saveToDo();
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  colorToDo(currentValue);
+  toDoInput.value = "";
+}
+function loadToDo() {
+  const loadedToDo = localStorage.getItem(TODO_LS);
+  if (loadedToDo !== null) {
+    const parsedToDo = JSON.parse(loadedToDo);
+    parsedToDo.forEach(function (toDoInput) {
+      colorToDo(toDoInput.text);
+    });
+  }
+}
+
+function init() {
+  loadToDo();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+init();
---
+++ weather.js
@@ -0,0 +1,50 @@
+const weather = document.querySelector(".js-weather");
+const COORDS = "coords";
+const API_KEY = "e1f8c7c7cf4e4b9b7cbcb71c1c52abf9";
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude,
+    longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+function handleGeoFail() {
+  console.log("Can't Access");
+}
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoFail);
+}
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
+  }
+}
+function init() {
+  loadCoords();
+}
+init();
