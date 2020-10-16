9106af12d6895cbb1a93cfeb86ed62de6d44a799
Youngmi <youngmi9347@gmail.com>
2020-10-16 10:31:16 +0900

    연도추가

--- clock.js
+++ clock.js
@@ -1,11 +1,17 @@
 const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
+  clockTitle = clockContainer.querySelector("h1"),
+  calenderTitle = clockContainer.querySelector("h3");
 
 function getTime() {
   const date = new Date();
   const minutes = date.getMinutes(),
     hours = date.getHours(),
-    seconds = date.getSeconds();
+    seconds = date.getSeconds(),
+    years = date.getFullYear(),
+    months = date.getMonth(),
+    days = date.getDay();
+
+  calenderTitle.innerText = `${years}년 ${months}월 ${days}일`;
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
